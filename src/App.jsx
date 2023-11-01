9e487e6df44f79c48d209e7d0b46a3315f78d385
nafisreza <bigganinafis@gmail.com>
2023-11-01 08:23:14 +0600

    ✨ useState hooks generated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(true)
+  const [charAllowed, setCharAllowed] = useState(true)
+  const [password, setPassowrd] = useState("")
 
   return (
-    <>
-      
-    </>
-  )
+<>
+</>
+  );
 }
 
-export default App
+export default App;
