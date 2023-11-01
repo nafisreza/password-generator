0fb8ce7b8ebafcaa2126e775bef2923712f0debb
nafisreza <bigganinafis@gmail.com>
2023-11-01 08:35:27 +0600

    🔒️ Password generator function created

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import "./App.css";
 
 function App() {
-  const [length, setLength] = useState(8)
-  const [numberAllowed, setNumberAllowed] = useState(true)
-  const [charAllowed, setCharAllowed] = useState(true)
-  const [password, setPassowrd] = useState("")
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(true);
+  const [charAllowed, setCharAllowed] = useState(true);
+  const [password, setPassword] = useState("");
 
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "*()&^%$#@!~{}<>?/`";
+
+    for (let i = 0; i < length; i++) {
+      let char = Math.round(Math.random * str.length + 1);
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassowrd]);
+  
   return (
-<>
-</>
-  );
+    <></>
+  )
 }
 
 export default App;
